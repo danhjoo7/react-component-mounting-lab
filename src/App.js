899bd8570d6da0799d49ac120e40516b81be8244
github username <github email address>
2019-11-23 09:54:08 -0800

    Done.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,9 @@ class App extends Component {
 
   //Your code here:
 
-
-
-
-
-
-
-
+  componentDidMount() {
+    this.handleAddTimer()
+  }
 
   // No need to modify anything in render or the class methods below
   // Unless, of course, you're curious about how it all works
@@ -36,6 +32,7 @@ class App extends Component {
       </div>
     );
   }
+
 
   // returns array of components written in JSX, mapped from this.state.timerIDs
   renderTimers = () => this.state.timerIDs.map(id => {
